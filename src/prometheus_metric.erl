6ed4cd496083117c573853742c949f90f08f0dda
Ilya Khaprov <dead.trickster@gmail.com>
2016-05-06 10:12:37 -0700

    remove unused stuff

--- src/prometheus_metric.erl
+++ src/prometheus_metric.erl
@@ -1,16 +1,8 @@
 -module(prometheus_metric).
--export([mf_metrics/2,
-         insert_mf/1,
+-export([insert_mf/1,
          check_mf_exists/4]).
 
 -include("prometheus.hrl").
-
-mf_metrics({Registry, Type, Name, Labels}, Callback) ->
-    [apply(Callback, [Name, Labels] ++ Metric) || Metric <- ets:match(metric_table(Type), {{Registry, Name, '$1'}, '$3'})].
-
-metric_table(counter) ->
-    ?PROMETHEUS_COUNTER_TABLE.
-
 insert_mf(MF) ->
     ets:insert_new(?PROMETHEUS_TABLE, MF).
 

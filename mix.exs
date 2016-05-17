f09cd248fc6c3457b7a9b5eabeb1ed2cd349c110
Ilya Khaprov <dead.trickster@gmail.com>
2016-05-16 23:32:20 -0700

    replace rebar3_hex with mix.exs

---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule Prometheus.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :prometheus,
+     version: "0.1.0",
+     description: description,
+     package: package]
+  end
+
+  defp description do
+    """
+    Prometheus monitoring system and time series database client in Erlang.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Ilya Khaprov"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/deadtrickster/prometheus.erl"},
+     files: ["src", "include", "README.md", "LICENSE", "rebar.config"]]
+  end
+end

f6e9bce562f65f3c218f73bc559ee143944c35ae
Ilya Khaprov <dead.trickster@gmail.com>
2016-05-18 03:58:36 -0700

    bump to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Prometheus.Mixfile do
 
   def project do
     [app: :prometheus,
-     version: "0.1.0",
+     version: "0.1.1",
      description: description,
      package: package]
   end

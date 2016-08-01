c6533757f43bd28eb8c38457bbe7560c1e67789b
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-01 14:02:24 +0300

    create_mf filters out undefined metrics; more metrics helpers

--- src/model/prometheus_model_helpers.erl
+++ src/model/prometheus_model_helpers.erl
@@ -5,18 +5,27 @@
          gauge_metrics/1,
          gauge_metric/1,
          gauge_metric/2,
+         counter_metrics/1,
          counter_metric/1,
          counter_metric/2,
+         summary_metrics/1,
+         summary_metric/2,
          summary_metric/3,
+         histogram_metrics/1,
+         histogram_metric/3,
          histogram_metric/4]).
 
 -include("prometheus_model.hrl").
+
+%%====================================================================
+%% Public API
+%%====================================================================
 
 create_mf(Name, Help, Type, Collector, CollectorData) ->
   #'MetricFamily'{name = ensure_binary(Name),
                   help = ensure_binary(Help),
                   type = ensure_mf_type(Type),
-                  metric = ensure_list(Collector:collect_metrics(Name, CollectorData))}.
+                  metric = filter_undefined_metrics(ensure_list(Collector:collect_metrics(Name, CollectorData)))}.
 
 gauge_metrics(Metrics) ->
   lists:map(fun(Spec) ->
@@ -38,6 +47,19 @@ gauge_metric(Labels, Value) ->
   #'Metric'{label = label_pairs(Labels),
             gauge = #'Gauge'{value=Value}}.
 
+counter_metrics(Metrics) ->
+  lists:map(fun(Spec) ->
+                case Spec of
+                  {Labels, Value} ->
+                    counter_metric(Labels, Value);
+                  {Value} ->
+                    counter_metric(Value);
+                  Value ->
+                    counter_metric(Value)
+                end
+            end,
+            Metrics).
+
 counter_metric(Value) ->
   counter_metric([], Value).
 
@@ -45,10 +67,38 @@ counter_metric(Labels, Value) ->
   #'Metric'{label = label_pairs(Labels),
             counter = #'Counter'{value=Value}}.
 
+summary_metrics(Metrics) ->
+  lists:map(fun(Spec) ->
+                case Spec of
+                  {Labels, Count, Sum} ->
+                    summary_metric(Labels, Count, Sum);
+                  {Count, Sum} ->
+                    summary_metric(Count, Sum)
+                end
+            end,
+            Metrics).
+
+summary_metric(Count, Sum) ->
+  summary_metric([], Count, Sum).
+
 summary_metric(Labels, Count, Sum) ->
   #'Metric'{label = label_pairs(Labels),
             summary = #'Summary'{sample_count=Count,
                                  sample_sum=Sum}}.
+
+histogram_metrics(Metrics) ->
+  lists:map(fun(Spec) ->
+                case Spec of
+                  {Labels, Buckets, Count, Sum} ->
+                    histogram_metric(Labels, Buckets, Count, Sum);
+                  {Buckets, Count, Sum} ->
+                    histogram_metric(Buckets, Count, Sum)
+                end
+            end,
+            Metrics).
+
+histogram_metric(Buckets, Count, Sum) ->
+  histogram_metric([], Buckets, Count, Sum).
 
 histogram_metric(Labels, Buckets, Count, Sum) ->
   #'Metric'{label = label_pairs(Labels),
@@ -56,6 +106,9 @@ histogram_metric(Labels, Buckets, Count, Sum) ->
                                      sample_sum=Sum,
                                      bucket=histogram_buckets(Buckets)}}.
 
+%%====================================================================
+%% Private Parts
+%%====================================================================
 
 label_pairs([]) ->
   [];
@@ -78,6 +131,9 @@ ensure_list(Val) when is_list(Val) ->
 ensure_list(Val) ->
   [Val].
 
+filter_undefined_metrics(Metrics) ->
+  lists:filter(fun(X) -> X /= undefined end, Metrics).
+
 ensure_binary(Val) when is_atom(Val) ->
   atom_to_binary(Val, utf8);
 ensure_binary(Val) when is_list(Val) ->
@@ -86,7 +142,6 @@ ensure_binary(Val) when is_binary(Val) ->
   Val;
 ensure_binary(Val) ->
   list_to_binary(io_lib:format("~p", [Val])).
-
 
 ensure_mf_type(gauge) ->
   'GAUGE';

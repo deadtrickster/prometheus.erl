721b1bcfb8cc5c9b73e941f061e620b7b72bd27f
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-15 14:08:00 +0300

    automate git pre-commit hook installation

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-git stash -q --keep-index
-./bin/checks.sh
-RESULT=$?
-git stash pop -q
-[ $RESULT -ne 0 ] && exit 1
-exit 0
+if [ $1 = "install" ]; then
+   ln -s `pwd`/bin/pre-commit.sh .git/hooks/pre-commit
+else
+    git stash -q --keep-index
+    ./bin/checks.sh
+    RESULT=$?
+    git stash pop -q
+    [ $RESULT -ne 0 ] && exit 1
+    exit 0
+fi

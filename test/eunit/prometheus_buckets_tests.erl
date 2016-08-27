7fecc1f0e8a0cb4ac4aedaba136963f848500fea
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-27 13:37:52 +0300

    [r][d] prometheus_buckets: remove deprecations
    generate_linear/3,
    generate_exponential/3

--- test/eunit/prometheus_buckets_tests.erl
+++ test/eunit/prometheus_buckets_tests.erl
@@ -2,20 +2,6 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-linear_generator_errors_test() ->
-  ?assertError({invalid_value, 0, "Buckets count should be positive"}, prometheus_buckets:generate_linear(-15, 5, 0)).
-
-linear_generator_test() ->
-  ?assertEqual([-15, -10, -5, 0, 5, 10], prometheus_buckets:generate_linear(-15, 5, 6)).
-
-exponential_generator_errors_test() ->
-  ?assertError({invalid_value, 0, "Buckets count should be positive"}, prometheus_buckets:generate_exponential(-15, 5, 0)),
-  ?assertError({invalid_value, -15, "Buckets start should be positive"}, prometheus_buckets:generate_exponential(-15, 5, 2)),
-  ?assertError({invalid_value, 0.5, "Buckets factor should be greater than 1"}, prometheus_buckets:generate_exponential(15, 0.5, 3)).
-
-exponential_generator_test() ->
-  ?assertEqual([100, 120, 144], prometheus_buckets:generate_exponential(100, 1.2, 3)).
-
 linear_errors_test() ->
   ?assertError({invalid_value, 0, "Buckets count should be positive"}, prometheus_buckets:linear(-15, 5, 0)).
 

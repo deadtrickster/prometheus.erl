17ee6ea05d61e403efedfbda5419851726c0ff3c
mats cronqvist <masse@cronqvi.st>
2016-09-25 14:41:15 +0200

    mnesia collector supports configurable list of metrics

--- src/collectors/mnesia/prometheus_mnesia_collector.erl
+++ src/collectors/mnesia/prometheus_mnesia_collector.erl
@@ -25,63 +25,79 @@ collect_mf(_Registry, Callback) ->
   end,
   ok.
 
-collect_mf(Callback) ->
+collect_mf(CB) ->
+  SetMetrics = application:get_env(prometheus,mnesia_collector_metrics, all),
+
   {Participants, Coordinators} =
-    case mnesia_tm:get_info(1000) of
+    case get_tm_info(SetMetrics) andalso mnesia_tm:get_info(1000) of
       {info, Ps, Cs} -> {length(Ps), length(Cs)};
       _ -> {undefined, undefined}    % gets transported as "NaN"
     end,
 
-  Callback(create_mf(erlang_mnesia_held_locks,
-                     "Number of held locks",
-                     gauge, ?MODULE,
-                     ets:info(mnesia_held_locks, size))),
-  Callback(create_mf(erlang_mnesia_lock_queue,
-                     "Number of transactions waiting for a lock",
-                     gauge, ?MODULE,
-                     ets:info(mnesia_lock_queue,size))),
-  Callback(create_mf(erlang_mnesia_transaction_participants,
-                     "Number of participant transactions",
-                     gauge, ?MODULE,
-                     Participants)),
-  Callback(create_mf(erlang_mnesia_transaction_coordinators,
-                     "Number of coordinator transactions",
-                     gauge, ?MODULE,
-                     Coordinators)),
-  Callback(create_mf(erlang_mnesia_failed_transactions,
-                     "Number of failed (i.e. aborted) transactions",
-                     counter, ?MODULE,
-                     mnesia:system_info(transaction_failures))),
-  Callback(create_mf(erlang_mnesia_committed_transactions,
-                     "Number of commited transactions",
-                     counter, ?MODULE,
-                     mnesia:system_info(transaction_commits))),
-  Callback(create_mf(erlang_mnesia_logged_transactions,
-                     "Number of transactions logged",
-                     counter, ?MODULE,
-                     mnesia:system_info(transaction_log_writes))),
-  Callback(create_mf(erlang_mnesia_restarted_transactions,
-                     "Total number of transaction restarts",
-                     counter, ?MODULE,
-                     mnesia:system_info(transaction_restarts))).
+  Metrics =
+    [{erlang_mnesia_held_locks,
+      held_locks,
+      "Number of held locks",
+      gauge,
+      fun() -> ets:info(mnesia_held_locks, size) end},
+     {erlang_mnesia_lock_queue,
+      lock_queue,
+      "Number of transactions waiting for a lock",
+      gauge,
+      fun() -> ets:info(mnesia_lock_queue,size) end},
+     {erlang_mnesia_transaction_participants,
+      transaction_participants,
+      "Number of participant transactions",
+      gauge,
+      fun() -> Participants end},
+     {erlang_mnesia_transaction_coordinators,
+      transaction_coordinators,
+      "Number of coordinator transactions",
+      gauge,
+      fun() -> Coordinators end},
+     {erlang_mnesia_failed_transactions,
+      transaction_failures,
+      "Number of failed (i.e. aborted) transactions",
+      counter,
+      fun() -> mnesia:system_info(transaction_failures) end},
+     {erlang_mnesia_committed_transactions,
+      transaction_commits,
+      "Number of commited transactions",
+      counter,
+      fun() -> mnesia:system_info(transaction_commits) end},
+     {erlang_mnesia_logged_transactions,
+      transaction_log_writes,
+      "Number of transactions logged",
+      counter,
+      fun() -> mnesia:system_info(transaction_log_writes) end},
+     {erlang_mnesia_restarted_transactions,
+      transaction_restarts,
+      "Total number of transaction restarts",
+      counter,
+      fun() -> mnesia:system_info(transaction_restarts) end}],
 
--define(COUNTERS,
-        [erlang_mnesia_failed_transactions,
-         erlang_mnesia_committed_transactions,
-         erlang_mnesia_logged_transactions,
-         erlang_mnesia_restarted_transactions]).
--define(GAUGES,
-        [erlang_mnesia_held_locks,
-         erlang_mnesia_lock_queue,
-         erlang_mnesia_transaction_participants,
-         erlang_mnesia_transaction_coordinators,
-         erlang_mnesia_current_transactions]).
+  lists:foreach(fun(Metric) -> emit(CB, Metric, SetMetrics) end, Metrics).
 
-collect_metrics(Key,Val) ->
-  case {lists:member(Key, ?COUNTERS), lists:member(Key, ?GAUGES)} of
-    {true, false} -> counter_metric(Val);
-    {false, true} -> gauge_metric(Val)
+get_tm_info(SetMetrics) ->
+  (SetMetrics =:= all)
+    orelse
+      (lists:member(transaction_participants, SetMetrics)
+       orelse
+       lists:member(transaction_coordinators, SetMetrics)).
+
+emit(CB, {MetricName, Selector, Help, Type, DF}, SetMetrics) ->
+  case SetMetrics =:= all orelse lists:member(Selector, SetMetrics) of
+    false -> ok;
+    true -> CB(create_mf(MetricName, Help, Type, ?MODULE, {Type, datafun(DF)}))
   end.
+
+datafun(DataFun) ->
+  try DataFun()
+  catch _:_ -> undefined
+  end.
+
+collect_metrics(_Key, {counter, Val}) -> counter_metric(Val);
+collect_metrics(_Key, {gauge, Val}) -> gauge_metric(Val).
 
 is_started(App) ->
   case [V || {A,_,V} <- application:which_applications(), A == App] of
--- test/eunit/collectors/mnesia/prometheus_mnesia_collector_tests.erl
+++ test/eunit/collectors/mnesia/prometheus_mnesia_collector_tests.erl
@@ -3,29 +3,42 @@
 -include_lib("eunit/include/eunit.hrl").
 
 prometheus_mnesia_off_test_() ->
-  {foreach,
+  {setup,
    fun prometheus_eunit_common:start/0,
    fun prometheus_eunit_common:stop/1,
-   [fun test_mnesia_off_collector/1]}.
+   [fun test_mnesia_off_collector/0]}.
 
-test_mnesia_off_collector(_) ->
+test_mnesia_off_collector() ->
   prometheus_registry:register_collector(prometheus_mnesia_collector),
   Metrics = prometheus_text_format:format(),
-  [
-   ?_assertMatch(nomatch, re:run(Metrics, "erlang_mnesia_held_locks"))
-  ].
+  ?assertMatch(nomatch, re:run(Metrics, "erlang_mnesia_held_locks")).
 
 prometheus_mnesia_on_test_() ->
-  {foreach,
+  {setup,
    fun() -> mnesia:start(),
             prometheus_eunit_common:start() end,
    fun(X) -> mnesia:stop(),
              prometheus_eunit_common:stop(X) end,
-   [fun test_mnesia_on_collector/1]}.
+   [fun test_mnesia_on_collector/0,
+    fun test_mnesia_on_collector_env_on/0,
+    fun test_mnesia_on_collector_env_off/0]}.
 
-test_mnesia_on_collector(_) ->
+test_mnesia_on_collector_env_on() ->
+  prometheus_registry:register_collector(prometheus_mnesia_collector),
+  application:set_env(prometheus,mnesia_collector_metrics,
+                      [transaction_coordinators]),
+  Metrics = prometheus_text_format:format(),
+  ?assertMatch({match,_}, re:run(Metrics, "erlang_mnesia_transaction_coord")),
+  application:unset_env(prometheus,mnesia_collector_metrics,[]).
+
+test_mnesia_on_collector_env_off() ->
+  prometheus_registry:register_collector(prometheus_mnesia_collector),
+  application:set_env(prometheus,mnesia_collector_metrics,[]),
+  Metrics = prometheus_text_format:format(),
+  ?assertMatch(nomatch, re:run(Metrics, "erlang_mnesia_held_locks")),
+  application:unset_env(prometheus,mnesia_collector_metrics,[]).
+
+test_mnesia_on_collector() ->
   prometheus_registry:register_collector(prometheus_mnesia_collector),
   Metrics = prometheus_text_format:format(),
-  [
-   ?_assertMatch({match, _}, re:run(Metrics, "erlang_mnesia_held_locks"))
-  ].
+  ?assertMatch({match, _}, re:run(Metrics, "erlang_mnesia_held_locks")).

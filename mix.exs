da2009477a7f6968011d2e132609dae69e04ac08
Ilya Khaprov <dead.trickster@gmail.com>
2016-09-27 13:52:09 +0300

    bump to 3.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Prometheus.Mixfile do
 
   def project do
     [app: :prometheus,
-     version: "3.0.0-rc1",
+     version: "3.0.0",
      description: description,
      package: package]
   end

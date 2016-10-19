e8be340b4e2956c9aa89f762fb6818509bf5386d
Ilya Khaprov <dead.trickster@gmail.com>
2016-10-19 15:40:16 +0300

    http content-type negotiation. closes #29

--- src/contrib/prometheus_http.erl
+++ src/contrib/prometheus_http.erl
@@ -2,7 +2,8 @@
 -module(prometheus_http).
 
 -export([microseconds_duration_buckets/0,
-         status_class/1]).
+         status_class/1,
+         negotiate/2]).
 
 -export_type([status_code/0,
               status_class/0]).
@@ -13,6 +14,11 @@
 
 -type status_code() :: pos_integer().
 -type status_class() :: prometheus_model_helpers:label_value().
+
+-record(media_range, {type,
+                      subtype,
+                      q,
+                      params}).
 
 %%====================================================================
 %% Public API
@@ -78,3 +84,193 @@ status_class(SCode) when is_integer(SCode)
   "unknown";
 status_class(C) ->
   erlang:error({invalid_value, C, "status code must be a positive integer"}).
+
+
+%% @doc
+%% Negotiate the most appropriate content_type given the accept header
+%% and a list of alternatives.
+%%
+%% Ported from [goautoneg](https://bitbucket.org/ww/goautoneg).
+%% ```
+%% Copyright (c) 2011, Open Knowledge Foundation Ltd.
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are
+%% met:
+%%
+%%     Redistributions of source code must retain the above copyright
+%%     notice, this list of conditions and the following disclaimer.
+%%
+%%     Redistributions in binary form must reproduce the above copyright
+%%     notice, this list of conditions and the following disclaimer in
+%%     the documentation and/or other materials provided with the
+%%     distribution.
+%%
+%%     Neither the name of the Open Knowledge Foundation Ltd. nor the
+%%     names of its contributors may be used to endorse or promote
+%%     products derived from this software without specific prior written
+%%     permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%% A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%% HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+%% LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+%% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%% '''
+%% @end
+-spec negotiate(Header, Alternatives) -> Match when
+    Header :: BinaryOrString,
+    Alternatives :: [Alternative],
+    Alternative :: BinaryOrString | {BinaryOrString | Tag},
+    BinaryOrString :: binary() | string(),
+    Tag :: any(),
+    Match :: Tag | nomatch.
+negotiate(Header, Alternatives) ->
+  Alts = lists:map(fun (Alt) ->
+                       {A, Tag} = case Alt of
+                                    {_, _} -> Alt;
+                                    _ -> {Alt, Alt}
+                                  end,
+                       {parse_media_range(ensure_string(A)), Tag}
+                   end,
+                   Alternatives),
+
+  MediaRanges = parse_accept(ensure_string(Header)),
+
+  try_match_mr(MediaRanges, Alts).
+
+
+
+%%====================================================================
+%% Private Parts
+%%====================================================================
+
+try_match(Match, [E | Rest]) ->
+  case Match(E) of
+    nomatch -> try_match(Match, Rest);
+    V -> V
+  end;
+try_match(_Match, []) -> nomatch.
+
+try_match_mr(MRs, Alts) ->
+  try_match(fun (MR) ->
+                try_match_alt(MR, Alts)
+            end,
+            MRs).
+
+try_match_alt(MR, Alts) ->
+  TryMatchAlt = fun(Ctsp) ->
+                    {A, Tag} = Ctsp,
+                    case mr_match(MR, A) of
+                      true -> Tag;
+                      _  -> nomatch
+                    end
+                end,
+  try_match(TryMatchAlt, Alts).
+
+mr_match(MR, #media_range{type = Type,
+                          subtype = Subtype}) ->
+  case MR of
+    #media_range{type = Type,
+                 subtype = Subtype} ->
+      true;
+    #media_range{type = Type,
+                 subtype = "*"} ->
+      true;
+    #media_range{type = "*",
+                 subtype = "*"} ->
+      true;
+    _ ->
+      false
+  end.
+
+parse_accept(AcceptString) ->
+  lists:sort(fun compare_media_ranges/2,
+             lists:map(fun parse_media_range/1,
+                       string:tokens(AcceptString, ","))).
+
+parse_media_range(RawMRString) ->
+  [MR | RawParams] = string:tokens(RawMRString, ";"),
+
+  [Type, Subtype] = lists:map(fun string:strip/1, string:tokens(MR, "/")),
+
+  Params = lists:filtermap(fun parse_media_range_param/1, RawParams),
+
+  {Q, ParamsWOQ} = find_media_range_q(Params),
+
+  #media_range{type = Type,
+               subtype = Subtype,
+               q = Q,
+               params = ParamsWOQ}.
+
+parse_media_range_param(Param) ->
+  case string:tokens(Param, "=") of
+    [Name, Value] -> {true, {Name, Value}};
+    _ -> false %% simply ignore malformed Name=Value pairs
+  end.
+
+find_media_range_q(Params) ->
+  {parse_q(proplists:get_value("q", Params, "1")),
+   proplists:delete("q", Params)}.
+
+parse_q(Q) ->
+  try
+    case lists:member($., Q) of
+      true ->
+        list_to_float(Q);
+      false ->
+        list_to_integer(Q)
+    end
+  catch error:badarg ->
+      0
+  end.
+
+compare_media_ranges(#media_range{q = Q1} = Range1,
+                     #media_range{q = Q2} = Range2) ->
+
+  case Q1 of
+    _ when Q1 > Q2 ->
+      true;
+    Q2 ->
+      compare_media_types(Range1, Range2);
+    _ ->
+      false
+  end.
+
+compare_media_types(#media_range{type = "*",
+                                 params = Params1},
+                    #media_range{type = "*",
+                                 params = Params2}) ->
+  length(Params1) >= length(Params2);
+compare_media_types(#media_range{type = "*"},
+                    _) ->
+  false;
+compare_media_types(_,
+                    #media_range{type = "*"}) ->
+  true;
+compare_media_types(#media_range{subtype = "*",
+                                 params = Params1},
+                    #media_range{subtype = "*",
+                                 params = Params2}) ->
+  length(Params1) =< length(Params2);
+compare_media_types(#media_range{subtype = "*"},
+                    _) ->
+  false;
+compare_media_types(_,
+                    #media_range{subtype = "*"}) ->
+  true;
+compare_media_types(#media_range{params = Params1},
+                    #media_range{params = Params2}) ->
+  length(Params1) >= length(Params2).
+
+ensure_string(V) when is_list(V) ->
+  V;
+ensure_string(V) when is_binary(V) ->
+  binary_to_list(V).
--- test/eunit/contrib/prometheus_http_tests.erl
+++ test/eunit/contrib/prometheus_http_tests.erl
@@ -2,6 +2,15 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-define(CHROME_ACCEPT, "application/xml,application/xhtml+xml,"
+        "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5").
+
+-define(PROMETHEUS_ACCEPT, "application/vnd.google.protobuf;"
+        "proto=io.prometheus.client.MetricFamily;encoding=delimited;q=0.7,"
+        "text/plain;version=0.0.4;q=0.3,"
+        "application/json;schema=\"prometheus/telemetry\";version=0.0.2;q=0.2,"
+        "*/*;q=0.1").
+
 microseconds_duration_buckets_test() ->
   ?assertMatch([10, 25, 50, 100, 250, 500,
                 1000, 2500, 5000, 10000, 25000, 50000, 100000, 250000, 500000,
@@ -22,3 +31,33 @@ status_class_test() ->
   ?assertMatch("client-error", prometheus_http:status_class(455)),
   ?assertMatch("server-error", prometheus_http:status_class(565)),
   ?assertMatch("unknown", prometheus_http:status_class(655)).
+
+
+%% https://bitbucket.org/ww/goautoneg
+
+content_negotiation_test() ->
+  ?assertEqual("image/png",
+               prometheus_http:negotiate(?CHROME_ACCEPT, ["text/html",
+                                                          "image/png"])),
+  ?assertEqual("text/html",
+               prometheus_http:negotiate(?CHROME_ACCEPT, ["text/html",
+                                                          "text/plain",
+                                                          "text/n3"])),
+  ?assertEqual("text/plain",
+               prometheus_http:negotiate(?CHROME_ACCEPT, ["text/n3",
+                                                          "text/plain"])),
+  ?assertEqual("text/n3",
+               prometheus_http:negotiate(?CHROME_ACCEPT, ["text/n3",
+                                                          "application/rdf+xml"])),
+
+  ?assertEqual(prometheus_protobuf_format,
+               prometheus_http:negotiate(?PROMETHEUS_ACCEPT,
+                                         [{prometheus_text_format:content_type(),
+                                           prometheus_text_format},
+                                          {prometheus_protobuf_format:content_type(),
+                                           prometheus_protobuf_format}])),
+
+  ?assertEqual(prometheus_text_format,
+               prometheus_http:negotiate(?PROMETHEUS_ACCEPT,
+                                         [{prometheus_text_format:content_type(),
+                                           prometheus_text_format}])).

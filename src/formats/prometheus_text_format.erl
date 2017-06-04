4fee420835b91daffccd7767081a08499fcb4511
Ilya Khaprov <dead.trickster@gmail.com>
2017-06-04 21:19:39 +0300

    reinstate 18.3 support

--- src/formats/prometheus_text_format.erl
+++ src/formats/prometheus_text_format.erl
@@ -143,7 +143,7 @@ labels_string(Labels) ->
   Fun = fun (#'LabelPair'{name=Name, value=Value}) ->
             [Name, "=\"", escape_label_value(Value), "\""]
         end,
-  ["{", lists:join(",", lists:map(Fun, Labels)), "}"].
+  ["{", join(",", lists:map(Fun, Labels)), "}"].
 
 emit_series(Fd, Name, Labels, undefined) ->
   LString = labels_string(Labels),
@@ -175,3 +175,33 @@ escape_label_value(Value) ->
 sub(Str, Old, New) ->
   RegExp = "\\Q" ++ Old ++ "\\E",
   re:replace(Str, RegExp, New, [global, {return, list}]).
+
+%%
+%% %CopyrightBegin%
+%%
+%% Copyright Ericsson AB 1996-2016. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% %CopyrightEnd%
+%%
+-spec join(Sep, List1) -> List2 when
+      Sep :: T,
+      List1 :: [T],
+      List2 :: [T],
+    T :: term().
+
+join(Sep, [H|T]) -> [H|join_prepend(Sep, T)].
+
+join_prepend(_Sep, []) -> [];
+join_prepend(Sep, [H|T]) -> [Sep, H|join_prepend(Sep, T)].

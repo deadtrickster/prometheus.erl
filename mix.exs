794245d0e3085a63a88b44f2454e84046accb23a
Ilya Khaprov <dead.trickster@gmail.com>
2017-12-10 17:20:18 +0300

    add prometheus-cowboy link to app metadata

--- mix.exs
+++ mix.exs
@@ -22,13 +22,14 @@ defmodule Prometheus.Mixfile do
     [build_tools: ["rebar3", "mix"],
      maintainers: ["Ilya Khaprov"],
      licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/deadtrickster/prometheus.erl",
-              "Inets HTTPD Exporter" => "https://hex.pm/packages/prometheus_httpd",
-              "Prometheus.ex" => "https://hex.pm/packages/prometheus_ex",
+     links: %{"Cowboy exporter/instrumenter" => "https://hex.pm/packages/prometheus_cowboy",
               "Ecto Instrumenter" => "https://hex.pm/packages/prometheus_ecto",
+              "GitHub" => "https://github.com/deadtrickster/prometheus.erl",
+              "Inets HTTPD Exporter" => "https://hex.pm/packages/prometheus_httpd",
               "Phoenix Instrumenter" => "https://hex.pm/packages/prometheus_phoenix",
               "Plugs Instrumenter/Exporter" => "https://hex.pm/packages/prometheus_plugs",
-              "Process info Collector" => "https://hex.pm/packages/prometheus_process_collector"},
+              "Process info Collector" => "https://hex.pm/packages/prometheus_process_collector",
+              "Prometheus.ex" => "https://hex.pm/packages/prometheus_ex"},
      files: ["mix.exs", "bin", "src", "include", "README.md", "LICENSE", "rebar.config"]]
   end
 end

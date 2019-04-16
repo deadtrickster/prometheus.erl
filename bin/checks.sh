46ea4b487baf4f6cc44495eae07582808e0369d4
Ilya Khaprov <i.khaprov@gmail.com>
2019-04-16 19:16:39 +0200

    use up-to-date rebar3

--- bin/checks.sh
+++ bin/checks.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-
-./elvis rock && rebar3 do xref, dialyzer, eunit
+mkdir _build
+curl -L -o ./_build/rebar3 https://s3.amazonaws.com/rebar3/rebar3
+chmod +x ./_build/rebar3
+./elvis rock && ./_build/rebar3 do xref, dialyzer, eunit

781c88cccce79c3c771c3f85b07998a3317e1180
Ilya Khaprov <i.khaprov@gmail.com>
2021-04-12 09:48:18 +0200

    Bump to v4.7.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Prometheus.Mixfile do
 
   def project do
     [app: :prometheus,
-     version: "4.6.0",
+     version: "4.7.0",
      description: description(),
      package: package()]
   end
